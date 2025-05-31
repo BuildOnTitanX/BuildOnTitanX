8b440c8a25b83030ab691b008683859aabbdb07b
Nona Median <median.nona@gmail.com>
2025-05-31 10:20:39 +0200

    add entries

--- src/content.config.mjs
+++ src/content.config.mjs
@@ -14,5 +14,59 @@ const projects = defineCollection({
   }),
 })
 
+// Define your "entries" collection schema, adjust fields as needed
+const entries = defineCollection({
+  schema: z.object({
+    id: z.string(),
+    published: z.boolean().default(true),
+    description: z.string().optional(),
+    title: z.string(),
+    blocks: z
+      .array(
+        z.discriminatedUnion('_block', [
+          z.object({
+            _block: z.literal('hero'),
+            heading: z.string(),
+            subheading: z.string().optional(),
+            image: z.string(),
+          }),
+          z.object({
+            _block: z.literal('card'),
+            title: z.string(),
+            description: z.string().optional(),
+            image: z.string().optional(),
+          }),
+          z.object({
+            _block: z.literal('code'),
+            language: z.string().default('javascript'),
+            code: z.string(),
+          }),
+          z.object({
+            _block: z.literal('pageheader'),
+            title: z.string(),
+            subtitle: z
+              .object({
+                content: z.string(),
+              })
+              .optional(),
+            bgType: z.enum(['default', 'bordered', 'filled']).default('default'),
+          }),
+          z.object({
+            _block: z.literal('accordion'),
+            useChevron: z.boolean().default(false),
+            allowMultiple: z.boolean().default(false),
+            items: z.array(
+              z.object({
+                title: z.string(),
+                content: z.string(),
+              }),
+            ),
+          }),
+        ]),
+      )
+      .optional(),
+  }),
+})
+
 // 4. Export a single `collections` object to register you collection(s)
-export const collections = { projects }
+export const collections = { projects, entries }
