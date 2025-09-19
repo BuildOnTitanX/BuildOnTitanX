f97c96a40a988fd52d70b341c9b9cd17b16db395
Nona Median <median.nona@gmail.com>
2025-09-19 08:20:05 +0200

    Add a first version of the about page, needing some better handling of the text-block, also sort the content showing the direct pages before the grouped ones.

--- src/content.config.mjs
+++ src/content.config.mjs
@@ -20,6 +20,7 @@ const pages = defineCollection({
   schema: z.object({
     title: z.string(),
     published: z.boolean().default(true),
+    linkUrl: z.string().optional(),
     description: z.string().optional(),
     blocks: z.array(z.any()).optional(),
   }),
